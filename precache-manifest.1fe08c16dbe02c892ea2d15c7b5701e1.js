707de70af20b05c448c48ef5bdc8358ad47c219f
wiem09 <62263147+wiem09@users.noreply.github.com>
2020-08-23 23:01:34 +0100

    Updates

---
+++ precache-manifest.1fe08c16dbe02c892ea2d15c7b5701e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79b499c56d91d35fcbc21c8e1c81cffa",
+    "url": "/Frontend/index.html"
+  },
+  {
+    "revision": "c7533433d0743b1a482f",
+    "url": "/Frontend/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c69c894df23546079913",
+    "url": "/Frontend/static/js/2.92b1f7a7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Frontend/static/js/2.92b1f7a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7533433d0743b1a482f",
+    "url": "/Frontend/static/js/main.8a10ddaf.chunk.js"
+  },
+  {
+    "revision": "6810d190fc647092dfd1",
+    "url": "/Frontend/static/js/runtime-main.c8c96d9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
